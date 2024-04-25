205a3629c61db263d2ac02508c61688cd2acfe33
jaycelab <jayxalvarado@gmail.com>
2024-04-25 15:35:36 -0700

    finished setting up mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
 // ensure to add .js since type is set to module
 import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(6900, () => {
-  console.log("Server is running on port 6900");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectMongoDB();
 });
