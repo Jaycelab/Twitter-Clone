7628d262bb65a9855cb6acc26ac4159f7c537130
jaycelab <jayxalvarado@gmail.com>
2024-04-30 08:35:18 -0700

    Finished user create post

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,13 +1,41 @@
+import Post from "../models/post.model.js";
 import User from "../models/user.model.js";
+import { v2 as cloudinary } from "cloudinary";
 
 export const createPost = async (req, res) => {
   try {
+    //let variable to revise
     const { text } = req.body;
     let { img } = req.body;
     const userId = req.user._id.toString();
 
-    //
+    //check if user exists, if not return error
     const user = await User.findById(userId);
-    if (!user) return res.status(404).json({ error: "User not found" });
-  } catch (error) {}
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    //check if text or image is provided
+    if (!text && !img) {
+      return res.status(400).json({ error: "Post must have text or image" });
+    }
+
+    //if image is provided, upload it to cloudinary
+
+    if (img) {
+      const uploadedResponse = await cloudinary.uploader.upload(img);
+      img = uploadedResponse.secure_url;
+    }
+
+    //saves new post to database and returns success created message
+    const newPost = new Post({
+      user: userId,
+      text,
+      img,
+    });
+
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+    console.log("Error in createPost controller: ", error);
+  }
 };
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
+import { createPost } from "../controllers/post.controller.js";
+
 const router = express.Router();
 
 //create post, delete post, like unlike post, comment on post
