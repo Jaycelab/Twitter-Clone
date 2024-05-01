cd6141e1e9707038a016f47d6f81d31076a6b63f
jaycelab <jayxalvarado@gmail.com>
2024-04-30 17:10:27 -0700

    update README

---
+++ README.md
@@ -0,0 +1,13 @@
+# X (twitter) clone for dogs and cats.
+
+## Tech Stack: React.js, MongoDB, Node.js, Express, Tailwind
+
+Features:
+
+- User authentication
+- React query for data fetching, caching, etc
+- Suggest users to follow
+- Create, delete, comment and like posts
+- Edit profile info, profile image and cover image
+- Dynamic cloud image user upload
+- Alert notifications
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -5,6 +5,8 @@ import { createPost } from "../controllers/post.controller.js";
 const router = express.Router();
 
 //create post, delete post, like unlike post, comment on post
+//***********************************************************/
+
 router.post("/create", protectRoute, createPost);
 //router.delete("/create", protectRoute, deletePost);
 //router.post("/like/:id", protectRoute, likeUnlikePost);
