300afa1db55fd7795898dcf1011c32e1b329a813
jaycelab <jayxalvarado@gmail.com>
2024-05-09 02:13:56 -0700

    image cleanup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/notifications" element={<NotificationPage />} />
-        <Route path="/profile/:username" element={<ProfilePage />} />
+        <Route path="/profile/ThatBoySpark" element={<ProfilePage />} />
       </Routes>
       <RightPanel />
     </div>
